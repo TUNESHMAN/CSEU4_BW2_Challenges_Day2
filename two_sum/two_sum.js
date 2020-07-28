e4ec61cd7690f20cf9fffb5892e3a4591b43151f
Babatunde <babatundea15@gmail.com>
2020-07-28 14:26:30 +0100

    fixed two-sum

--- two_sum/two_sum.js
+++ two_sum/two_sum.js
@@ -1,8 +1,19 @@
+let nums = [1, 3, 5, 2, 4, 7];
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    let element = nums[i];
+    for (let j = 1; j < nums.length; j++) {
+      let secondElement = nums[j];
+      if (element + secondElement === target) {
+        return [i, j];
+      }
+    }
+  }
 };
+
+console.log(twoSum(nums, 7));
